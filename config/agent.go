66ca7cb7081d8ca86de021d64db218f3d9ef5d35
Christian Mauduit (DataDog) <christian.mauduit@datadoghq.com>
2017-02-24 15:21:13 +0100

    [bench] collecting data for ExtraSampleRate=0.5

--- config/agent.go
+++ config/agent.go
@@ -126,7 +126,7 @@ func NewDefaultAgentConfig() *AgentConfig {
 		BucketInterval:   time.Duration(10) * time.Second,
 		ExtraAggregators: []string{},
 
-		ExtraSampleRate: 0.01,
+		ExtraSampleRate: 0.5,
 		MaxTPS:          10000000,
 
 		ReceiverHost:    "localhost",
