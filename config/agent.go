9fb45196679de5d0125dc8a0bb74ff1d6d4323ba
Christian Mauduit (DataDog) <christian.mauduit@datadoghq.com>
2017-03-02 17:19:55 +0100

    [watchdog] refactoring, mostly simplified some test assertions

--- config/agent.go
+++ config/agent.go
@@ -141,7 +141,7 @@ func NewDefaultAgentConfig() *AgentConfig {
 
 		MaxMemory:        1e9,
 		MaxConnections:   5000,
-		WatchdogInterval: time.Duration(1) * time.Minute,
+		WatchdogInterval: time.Minute,
 	}
 
 	return ac
